20296565e66dd8d2a5b4ee5ee89a9a51492582aa
Mateusz Palyz <mateusz.palyz@netguru.pl>
2017-10-08 11:54:22 +0200

    if user logged in, redirect to root

--- src/Login.js
+++ src/Login.js
@@ -21,14 +21,12 @@ class Login extends React.Component {
   }
 
   render() {
+    if (fakeAuth.isAuthenticated) { return <Redirect to='/' /> }
+
     const { from } = this.props.location.state || { from: { pathname: '/' } }
     const { redirectToReferrer } = this.state
 
-    if (redirectToReferrer) {
-      return (
-        <Redirect to={from}/>
-      )
-    }
+    if (redirectToReferrer) { return <Redirect to={from} /> }
 
     return (
       <div>
